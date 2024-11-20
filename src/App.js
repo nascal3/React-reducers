c4d75cf2c72f4be45017bfb7fc757fe9ecc3586d
Caleb Nasio <nascal3@gmail.com>
2024-11-20 14:26:18 +0300

    State management using reducers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {useReducer, useState} from "react";
 
 function App() {
+
+  const [count, setCount] = useState(0)
+
+    const [state, dispatch] = useReducer(reducer, {count: 0, incrementBy: 1})
+
+    function reducer(state, action) {
+        const map = {
+                'increment': {...state, count: state.count + action.payload + state.incrementBy},
+                'decrement': {...state, count: state.count - action.payload - state.incrementBy},
+                'setIncrementBy': {...state, incrementBy: action.payload}
+            }
+
+        return map[action.type]
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <h2>useState: {count}</h2>
+          <button onClick={() => setCount(count + 1)}>Increase</button>
+          <button onClick={() => setCount(count - 1)}>Decrease</button>
+
+          <h2>reducer: {state.count}</h2>
+          <input value={state.incrementBy}
+            onChange={(e)=> dispatch({type: "setIncrementBy", payload: Number(e.target.value)})}
+            type="text"
+          />
+          <button onClick={() => dispatch({ type: "increment", payload: 1 })}>Increase</button>
+          <button onClick={() => dispatch({ type: "decrement", payload: 1 })}>Decrease</button>
+      </div>
   );
 }
 
